04055de172322fe096e67d914a4238cf2ca5d7e5
strider744 <strider744@gmail.com>
2020-10-03 02:20:16 +0500

    footer and header  done

---
+++ app/src/main/java/com/strider/imagesearchapp/ui/gallery/PhotoLoadStateAdapter.kt
@@ -0,0 +1,47 @@
+package com.strider.imagesearchapp.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.strider.imagesearchapp.databinding.PhotoLoadStateFooterBinding
+
+class PhotoLoadStateAdapter(private val retry: () -> Unit) :
+    LoadStateAdapter<PhotoLoadStateAdapter.LoadStateViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
+        val binding = PhotoLoadStateFooterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return LoadStateViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    inner class LoadStateViewHolder(private val binding: PhotoLoadStateFooterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.buttonRetry.setOnClickListener {
+                retry.invoke()
+            }
+        }
+
+        fun bind(loadState: LoadState) {
+            binding.apply {
+                progressBar.isVisible = loadState is LoadState.Loading
+                buttonRetry.isVisible = loadState !is LoadState.Loading
+                tvError.isVisible = loadState !is LoadState.Loading
+            }
+        }
+
+    }
+
+}
