fb09736806e104108e492ef16590b79813b0b1c9
strider744 <strider744@gmail.com>
2020-10-02 20:31:57 +0500

    finish photo data class

---
+++ app/src/main/java/com/strider/imagesearchapp/Photo.kt
@@ -0,0 +1,29 @@
+package com.strider.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Photo(
+    val id: String,
+    val description: String?,
+    val urls: PhotoUrls,
+    val user: PhotoUser
+) : Parcelable {
+    @Parcelize
+    data class PhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class PhotoUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=SimpleImageSearchApp&utm_medium=referral"
+    }
+}
